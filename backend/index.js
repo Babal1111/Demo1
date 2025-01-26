1e16d6d72ff4e0ff0f3fa74cfefb0bee223cb268
Babal1111 <168063358+Babal1111@users.noreply.github.com>
2025-01-26 19:46:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,55 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const cors = require('cors');
+// const express = require("express");
+// const { MongoClient } = require("mongodb");
+// const cors = require('cors');
 
-const app = express();
-// app.use(cors());
-const corsOptions = {
-    origin: ["https://demo1-frontend.vercel.app/"], // Frontend URL
-    methods: ["GET,POST"], // Specify allowed methods if needed
-    credentials:true // Specify allowed headers if needed
-  };
+// const app = express();
+// // app.use(cors());
+// const corsOptions = {
+//     origin: ["https://demo1-frontend.vercel.app/"], // Frontend URL
+//     methods: ["GET,POST"], // Specify allowed methods if needed
+//     credentials:true // Specify allowed headers if needed
+//   };
   
-  app.use(cors(corsOptions));
+//   app.use(cors(corsOptions));
 
-app.use(express.json());
+// app.use(express.json());
 
-const PORT_REST = 5000; 
+// const PORT_REST = 5000; 
 
-const DB_URI = "mongodb+srv://babalpreet102:Babal1111@questions.upcav.mongodb.net/?retryWrites=true&w=majority&appName=questions";
-const DB_NAME = "questions"; 
-const COLLECTION_NAME = "questionare"; 
+// const DB_URI = "mongodb+srv://babalpreet102:Babal1111@questions.upcav.mongodb.net/?retryWrites=true&w=majority&appName=questions";
+// const DB_NAME = "questions"; 
+// const COLLECTION_NAME = "questionare"; 
 
-let db;
-MongoClient.connect(DB_URI)
-  .then((client) => {
-    db = client.db(DB_NAME);
-    console.log(`Connected to MongoDB database: ${DB_NAME}`);
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
-app.get("/", (req, res) => {
-  res.send("API is working.on vercel!");
-});
+// let db;
+// MongoClient.connect(DB_URI)
+//   .then((client) => {
+//     db = client.db(DB_NAME);
+//     console.log(`Connected to MongoDB database: ${DB_NAME}`);
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connection error:", err);
+//   });
+// app.get("/", (req, res) => {
+//   res.send("API is working.on vercel!");
+// });
 
 
-app.get("/questions", async (req, res) => {
-  try {
-    const { query } = req.query;
-    const questions = await db
-      .collection(COLLECTION_NAME)
-      .find({ title: { $regex: query, $options: "i" } })
-      .toArray();
-    res.json(questions);
-  } catch (error) {
-    console.error("Error fetching questions:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// app.get("/questions", async (req, res) => {
+//   try {
+//     const { query } = req.query;
+//     const questions = await db
+//       .collection(COLLECTION_NAME)
+//       .find({ title: { $regex: query, $options: "i" } })
+//       .toArray();
+//     res.json(questions);
+//   } catch (error) {
+//     console.error("Error fetching questions:", error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-app.listen(PORT_REST, () => {
-  console.log(`Server running on port ${PORT_REST}`);
-});
+// app.listen(PORT_REST, () => {
+//   console.log(`Server running on port ${PORT_REST}`);
+// });
 
 
