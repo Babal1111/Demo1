31785d48b7716316fd6057720853be5b8dc818ae
Babalpreet <babaldon1111@gmail.com>
2025-01-26 16:30:13 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+const PORT_REST = 5000; 
+
+const DB_URI = "mongodb+srv://babalpreet102:Babal1111@questions.upcav.mongodb.net/?retryWrites=true&w=majority&appName=questions";
+const DB_NAME = "questions"; 
+const COLLECTION_NAME = "questionare"; 
+
+let db;
+MongoClient.connect(DB_URI)
+  .then((client) => {
+    db = client.db(DB_NAME);
+    console.log(`Connected to MongoDB database: ${DB_NAME}`);
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+app.get("/questions", async (req, res) => {
+  try {
+    const { query } = req.query;
+    const questions = await db
+      .collection(COLLECTION_NAME)
+      .find({ title: { $regex: query, $options: "i" } })
+      .toArray();
+    res.json(questions);
+  } catch (error) {
+    console.error("Error fetching questions:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT_REST, () => {
+  console.log(`Server running on port ${PORT_REST}`);
+});
+
+
